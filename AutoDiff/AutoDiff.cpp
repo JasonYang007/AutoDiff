e07c31e05739fa7bdb40bfe2b4f1c2a5efb5695e
Jason Yang <jhwan.yang@gmail.com>
2014-01-26 14:55:56 -0500

    astyle formatted

--- AutoDiff/AutoDiff.cpp
+++ AutoDiff/AutoDiff.cpp
@@ -12,11 +12,11 @@ void nop()
 template<class T, class E>
 T EvaluateValueAdjoint(ADExpr<T,E> const & ee)
 {
-	ADExpr<T,E> & expr = const_cast<ADExpr<T,E> &>(ee);
-	T res = expr.EvaluateValue();
-	expr.SetAdjoint(1.0);
-	expr.EvaluateAdjoint();
-	return res;
+    ADExpr<T,E> & expr = const_cast<ADExpr<T,E> &>(ee);
+    T res = expr.EvaluateValue();
+    expr.SetAdjoint(1.0);
+    expr.EvaluateAdjoint();
+    return res;
 }
 
 template<class T>
@@ -27,38 +27,38 @@ void arg_test(T const & x)
 template<class E1, class E2>
 void PrintXY(E1 const & x, E2 const & y)
 {
-	std::cout << "x = " << x << std::endl;
-	std::cout << "y = " << y << std::endl;
+    std::cout << "x = " << x << std::endl;
+    std::cout << "y = " << y << std::endl;
 }
 
 void test_func_add(double xx, double yy)
 {
-	ADVar<double> x=xx;
-	ADRandVar<double> y=yy;
+    ADVar<double> x=xx;
+    ADRandVar<double> y=yy;
 
-	ADVar<double> res;
-	res.SetAsRoot();
-	EvaluateValueAdjoint( res = x * x - y );
-	std::cout << "x*x-y = " << res << std::endl;
-	PrintXY(x,y);
+    ADVar<double> res;
+    res.SetAsRoot();
+    EvaluateValueAdjoint( res = x * x - y );
+    std::cout << "x*x-y = " << res << std::endl;
+    PrintXY(x,y);
 }
 
 void test_func_mult(double xx, double yy)
 {
-	ADVar<double> x=xx;
-	ADVar<double> y=yy;
-	ADArray<double> z(2);
-	ADVar<double> res;
-	res.SetAsRoot();
+    ADVar<double> x=xx;
+    ADVar<double> y=yy;
+    ADArray<double> z(2);
+    ADVar<double> res;
+    res.SetAsRoot();
 
-	EvaluateValueAdjoint((
-		z[0] = x*x
-		, z[1] = z[0] * (x + y)
-		, res = z[1] / y
-	));
+    EvaluateValueAdjoint((
+                             z[0] = x*x
+                                    , z[1] = z[0] * (x + y)
+                                            , res = z[1] / y
+                         ));
 
-	std::cout << "x*x*(x+y)/y = " << res << std::endl;
-	PrintXY(x,y);
+    std::cout << "x*x*(x+y)/y = " << res << std::endl;
+    PrintXY(x,y);
 }
 
 //--- nested_apply
@@ -70,13 +70,13 @@ struct Func<ADExpr<T,E> >
 {
     typedef ADExpr<T,E> var_t;
     typedef EXP_ASSIGN(var_t,
-                EXP_DIV(
-                   EXP_MULT(
-                        EXP_MULT(var_t,var_t),
-                        EXP_PLUS(var_t,var_t)),
-                   var_t))
-            return_t;
-    
+                       EXP_DIV(
+                           EXP_MULT(
+                               EXP_MULT(var_t,var_t),
+                               EXP_PLUS(var_t,var_t)),
+                           var_t))
+    return_t;
+
     return_t operator()(var_t const & x, var_t const & y, ADVar<T> & z)
     {
         return z = (x*x)*(x+y)/y;
@@ -85,17 +85,17 @@ struct Func<ADExpr<T,E> >
 
 void test_func_nested(double xx, double yy)
 {
-	ADVar<double> x=xx;
-	ADVar<double> y=yy;
-	ADVar<double> z;
+    ADVar<double> x=xx;
+    ADVar<double> y=yy;
+    ADVar<double> z;
     z.SetAsRoot();
 
     EvaluateValueAdjoint((
-        Func<ADExpr<double,ExpVar> >()(x,y,z)
-    ));
-        
-	std::cout << "F(x) = " << z << std::endl;
-	PrintXY(x,y);
+                             Func<ADExpr<double,ExpVar> >()(x,y,z)
+                         ));
+
+    std::cout << "F(x) = " << z << std::endl;
+    PrintXY(x,y);
 }
 
 void test_func_lambda(double xx, double yy)
@@ -103,28 +103,28 @@ void test_func_lambda(double xx, double yy)
     ADPos<double,1> _1;
     ADPos<double,2> _2;
 
-	ADVar<double> x=xx;
-	ADRandVar<double> y=yy;
+    ADVar<double> x=xx;
+    ADRandVar<double> y=yy;
 
-	ADVar<double> z;
+    ADVar<double> z;
     z.SetAsRoot();
 
     EvaluateValueAdjoint((
-		z = (_1 * _2)(x,y)
-    ));
-	std::cout << "(_1 * _2)(x,y) = " << z << std::endl;
-	PrintXY(x,y);
+                             z = (_1 * _2)(x,y)
+                         ));
+    std::cout << "(_1 * _2)(x,y) = " << z << std::endl;
+    PrintXY(x,y);
 }
 
 void autodiff_test()
 {
-	double x=6.0;
-	double y=3.0;
-	
+    double x=6.0;
+    double y=3.0;
+
 //  test_func_add(x,y);
 //  test_func_mult(x,y);
 //  test_func_nested(x,y);
     test_func_lambda(x,y);
 
-	nop();
+    nop();
 }
--- AutoDiff/Main.cpp
+++ AutoDiff/Main.cpp
@@ -2,6 +2,6 @@ void autodiff_test();
 
 int main(int arrc, char * argv[])
 {
-	autodiff_test();
-	return 0;
+    autodiff_test();
+    return 0;
 }
